4e953a497054b3e83b733af04aa9ba8179c8ca08
rgarces <43771367+TheBeeProgrammer@users.noreply.github.com>
2021-06-05 16:20:00 -0500

    structure

--- movies/app/src/main/java/com/example/presentation/MovieActivity.kt
+++ movies/app/src/main/java/com/example/presentation/MovieActivity.kt
@@ -1,9 +1,10 @@
-package com.example.movies
+package com.example.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.movies.R
 
-class MainActivity : AppCompatActivity() {
+class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
