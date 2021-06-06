369e1cff40d53a8e568d226e0363ce01bccfa2e8
rgarces <43771367+TheBeeProgrammer@users.noreply.github.com>
2021-06-05 22:34:23 -0500

    added movie detail network configs

---
+++ movies/app/src/main/java/com/example/data/network/api/MovieDetailApi.kt
@@ -0,0 +1,17 @@
+package com.example.data.network.api
+
+import com.example.data.network.entities.moviedetail.MovieDetailResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+
+class MovieDetailApi(private var theMovieDbService: TheMovieDbService) {
+
+    suspend fun fetchMovies(movieId: Int) : Flow<MovieDetailResponse> {
+        return flow {
+            emit(theMovieDbService.fetchMovieDetail(837007))
+        }.flowOn(Dispatchers.IO)
+    }
+}
---
+++ movies/app/src/main/java/com/example/data/network/entities/moviedetail/MovieDetailResponse.kt
@@ -0,0 +1,16 @@
+package com.example.data.network.entities.moviedetail
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieDetailResponse(
+    val id: Int,
+    val overview: String,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("release_date")
+    val releaseDate: String,
+    val title: String,
+    @SerializedName("vote_average")
+    val voteAverage: Double,
+    val vote_count: Int
+)
