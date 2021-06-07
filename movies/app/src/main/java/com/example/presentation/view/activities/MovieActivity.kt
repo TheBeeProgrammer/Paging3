28e601089e3e48fe353ce4cfc5d9846ad2f40472
rgarces <43771367+TheBeeProgrammer@users.noreply.github.com>
2021-06-07 03:32:09 -0500

    added animations transactions

--- movies/app/src/main/java/com/example/presentation/view/activities/MovieActivity.kt
+++ movies/app/src/main/java/com/example/presentation/view/activities/MovieActivity.kt
@@ -19,7 +19,7 @@ class MovieActivity : AppCompatActivity(), MovieFragment.CallBack {
         if (currentFragment == null) {
             val fragment = MovieFragment.newInstance()
             supportFragmentManager
-                .beginTransaction()
+                .beginTransaction().setCustomAnimations(R.anim.slide_in, 0, 0, 0)
                 .add(R.id.fragmentContainer, fragment)
                 .commit()
         }
@@ -27,7 +27,9 @@ class MovieActivity : AppCompatActivity(), MovieFragment.CallBack {
 
     override fun navigateToMovieDetail(movieId: Int?) {
         val fragment = MovieDeatilFragment.newInstance(movieId)
-        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment)
+        supportFragmentManager.beginTransaction()
+            .setCustomAnimations(R.anim.slide_in, R.anim.fade_out, R.anim.fade_in, R.anim.slide_out)
+            .replace(R.id.fragmentContainer, fragment)
             .addToBackStack(null).commit()
     }
 
